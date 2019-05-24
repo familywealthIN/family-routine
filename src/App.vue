17b02739c35661317398d5dc43a85ce520e97af0
Gaurav Panchal <grvpanchal@gmail.com>
2019-05-25 02:58:08 +0530

    adding vue source

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app>
+  <v-navigation-drawer app>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </v-navigation-drawer>
+  <v-toolbar app></v-toolbar>
+  <v-content>
+      <router-view></router-view>
+  </v-content>
+  <v-footer app></v-footer>
+</v-app>
+</template>
+
