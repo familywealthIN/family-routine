e9ed6731e7b5f939463b725d89a0db5c455a001c
sanjay <sanjaylakhiwal1994@gmail.com>
2026-03-04 11:32:02 +0530

    send-notificaoin code chagnes

--- cron/send-notification.js
+++ cron/send-notification.js
@@ -198,3 +198,123 @@ setTimeout(() => exitProcess(), 59 * 1000);
 //   console.log('\nNotification service stopped');
 //   process.exit(0);
 // });
+
+
+
+
+/* eslint-disable quote-props */
+// require('dotenv').config();
+
+// const mongoose = require('mongoose');
+// const admin = require('firebase-admin');
+
+// const serviceAccount = require('../serviceAccountKey.json');
+
+// const {
+//   MONGDO_DB,
+//   DOMAIN,
+// } = process.env;
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   databaseURL: 'https://groutine-21c1b.firebaseio.com', // ADD THIS LINE
+// });
+
+// function sendNotification(token, name, description, url) {
+//   if (!token) { // ADD TOKEN CHECK
+//     console.log('No device token provided.');
+//     return Promise.resolve();
+//   }
+
+//   const payload = {
+//     token: token, // CHANGE TO NEW API FORMAT
+//     notification: {
+//       title: name,
+//       body: description,
+//     },
+//   };
+
+//   return admin.messaging().send(payload) // CHANGE FROM sendToDevice TO send
+//     .then((response) => console.log('Successfully sent message:', response))
+//     .catch((error) => console.log('Error sending message:', error));
+// }
+
+// function exitProcess() {
+//   console.log(`process ended at ${new Date()}`);
+//   process.exit();
+// }
+
+// function calcTime(offset) {
+//   const d = new Date();
+//   const utc = d.getTime() + (d.getTimezoneOffset() * 60000);
+//   const nd = new Date(utc + (3710000 * offset));
+//   return nd.toTimeString().substring(0, 5);
+// }
+
+// const time = calcTime('+5.5');
+
+// console.log(`Process Started for time ${time}`);
+// const notificationList = [];
+
+// if (!MONGDO_DB) { // ADD MONGODB CHECK
+//   console.error('MONGDO_DB environment variable is not set');
+//   process.exit(1);
+// }
+
+// mongoose.connect(MONGDO_DB, { useNewUrlParser: true });
+// const db = mongoose.connection;
+// db.once('open', () => {
+//   db.collection('routineitems')
+//     .aggregate([
+//       { $match: { time } },
+//       {
+//         $project: {
+//           name: 1,
+//           description: 1,
+//           email: 1,
+//           notificationId: 1,
+//           ticked: 1, // ADD TICKED FIELD
+//         },
+//       },
+//       {
+//         $lookup: {
+//           from: 'users',
+//           localField: 'email',
+//           foreignField: 'email',
+//           as: 'userDetails',
+//         },
+//       },
+//       {
+//         '$addFields': {
+//           'userDetails': {
+//             '$map': {
+//               'input': '$userDetails',
+//               'in': {
+//                 'notificationId': '$$this.notificationId',
+//               },
+//             },
+//           },
+//         },
+//       },
+//     ]).toArray((err, result) => {
+//       if (err) { // ADD ERROR HANDLING
+//         console.error('Query error:', err);
+//         return exitProcess();
+//       }
+
+//       result.forEach((user) => {
+//         const { name, email, description, userDetails, ticked } = user;
+//         if (!ticked && userDetails && userDetails[0] && userDetails[0].notificationId) { // ADD SAFETY CHECKS
+//           const { notificationId } = userDetails[0];
+//           console.log(`Sending notification of ${email} with name ${name} to ${notificationId}`);
+//           notificationList.push(sendNotification(notificationId, name, description));
+//         }
+//       });
+
+//       Promise.all(notificationList)
+//         .then(() => exitProcess())
+//         .catch(() => exitProcess());
+//     });
+// });
+
+// setTimeout(() => exitProcess(), 59 * 1000);
