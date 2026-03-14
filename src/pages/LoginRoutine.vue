2927167378336669f09c7e53c96ff159f4f79bc9
sanjay <sanjaylakhiwal1994@gmail.com>
2026-03-14 19:58:09 +0530

    login changes

--- src/pages/LoginRoutine.vue
+++ src/pages/LoginRoutine.vue
@@ -34,28 +34,29 @@
         </div>
       </a>
       <a href="javascript:void(0)" class="apple-button" alt="Login with Apple" @click="handleAppleSignIn"
-      v-if="!isSignIn && isIOS" :disabled="!isInit">
-      <div class="apple-box">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20px" height="20px" fill="#000">
-        <path d="M18.71 19.5c-.83 1.24-1.71 2.45-3.05 2.47-1.34.03-1.77-.79-3.29-.79-1.53 0-2 .77-3.27.82-1.31.05-2.3-1.32-3.14-2.53C4.25 17 2.94 12.45 4.7 9.39c.87-1.52 2.43-2.48 4.12-2.51 1.28-.02 2.5.87 3.29.87.78 0 2.26-1.07 3.81-.91.65.03 2.47.26 3.64 1.98-.09.06-2.17 1.28-2.15 3.81.03 3.02 2.65 4.03 2.68 4.04-.03.07-.42 1.44-1.38 2.83M13 3.5c.73-.83 1.94-1.46 2.94-1.5.13 1.17-.34 2.35-1.04 3.19-.69.85-1.83 1.51-2.95 1.42-.15-1.15.41-2.35 1.05-3.11z"/>
-      </svg>
-      </div>
-      <div class="apple-text">
-        Sign in with Apple
-      </div>
-    </a>
+        v-if="!isSignIn && isIOS" :disabled="!isInit">
+        <div class="apple-box">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20px" height="20px" fill="#000">
+            <path
+              d="M18.71 19.5c-.83 1.24-1.71 2.45-3.05 2.47-1.34.03-1.77-.79-3.29-.79-1.53 0-2 .77-3.27.82-1.31.05-2.3-1.32-3.14-2.53C4.25 17 2.94 12.45 4.7 9.39c.87-1.52 2.43-2.48 4.12-2.51 1.28-.02 2.5.87 3.29.87.78 0 2.26-1.07 3.81-.91.65.03 2.47.26 3.64 1.98-.09.06-2.17 1.28-2.15 3.81.03 3.02 2.65 4.03 2.68 4.04-.03.07-.42 1.44-1.38 2.83M13 3.5c.73-.83 1.94-1.46 2.94-1.5.13 1.17-.34 2.35-1.04 3.19-.69.85-1.83 1.51-2.95 1.42-.15-1.15.41-2.35 1.05-3.11z" />
+          </svg>
+        </div>
+        <div class="apple-text">
+          Sign in with Apple
+        </div>
+      </a>
     </div>
     <!-- <footer> -->
-      <div class="text-xs-center text-muted">
-        <small>
-          By using
-          Routine Notes,
-          you accept our
-          <a href="https://familywealth.in/terms">Terms</a>
-          and
-          <a href="https://familywealth.in/privacy-policy">Privacy Policy</a>.
-        </small>
-      </div>
+    <div class="text-xs-center text-muted">
+      <small>
+        By using
+        Routine Notes,
+        you accept our
+        <a href="https://familywealth.in/terms">Terms</a>
+        and
+        <a href="https://familywealth.in/privacy-policy">Privacy Policy</a>.
+      </small>
+    </div>
     <!-- </footer> -->
   </container-box>
 
@@ -252,6 +253,23 @@ export default {
       //       grantOfflineAccess: true,
       //       forceCodeForRefreshToken: true
       //     });
+        //   if(platform === 'ios'){
+        //         await GoogleAuth.initialize({
+        //           clientId: clientId,
+        //           scopes: ['profile', 'email'],
+        //           grantOfflineAccess: true,
+        //           forceCodeForRefreshToken: true
+        //         });
+
+        //   }else{
+        //   await GoogleAuth.initialize({
+        //     clientId: clientId,
+        //     scopes: ['profile', 'email'],
+        //     grantOfflineAccess: true,
+        //     forceCodeForRefreshToken: true
+        //   });
+        // }
+
           if(platform === 'ios'){
                 await GoogleAuth.initialize({
                   clientId: clientId,
@@ -262,9 +280,12 @@ export default {
 
           }else{
           await GoogleAuth.initialize({
-            clientId: clientId,
+            clientId: '350952942983-eu6bevc5ve0pjkfqarolulruhbokat05.apps.googleusercontent.com',
             scopes: ['profile', 'email'],
             grantOfflineAccess: true,
+            androidClientId: '350952942983-eu6bevc5ve0pjkfqarolulruhbokat05.apps.googleusercontent.com',
+            iosClientId: '350952942983-48lis9mbeudskd9rovrnov5gm35h0vre.apps.googleusercontent.com',
+            webClientId: '350952942983-eu6bevc5ve0pjkfqarolulruhbokat05.apps.googleusercontent.com',
             forceCodeForRefreshToken: true
           });
         }
@@ -628,7 +649,7 @@ export default {
 }
 
 /* Ensure content doesn't overlap with footer */
- /* .login-box {
+/* .login-box {
   margin-bottom: 80px;
 } */
 .apple-button {
@@ -673,5 +694,4 @@ export default {
   flex: 1;
   text-align: center;
 }
-
 </style>
